43a5e1cae27b7256e5a2fd6a4fd46ce0fc04d153
Sergii Proiava <seregikpro@gmail.com>
2022-08-03 16:21:50 +0300

    add hover to AppBar

--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -4,8 +4,13 @@ import { NavLink } from 'react-router-dom';
 export const List = styled.ul`
   display: flex;
   padding-bottom: 20px;
-  box-shadow: 0px 3px 2px 2px rgba(100, 100, 150, 0.15);
-  border-bottom: solid 1px #fff;
+
+  border-bottom: solid 1px rgba(100, 100, 150, 0.15);
+  transition: box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  :hover {
+    box-shadow: 0px 3px 2px 2px rgba(100, 100, 150, 0.15);
+  }
 `;
 
 export const Navlink = styled(NavLink)`
